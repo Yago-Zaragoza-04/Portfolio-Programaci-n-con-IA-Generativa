40ba63e40721d805a91bfc42bc7772c876c6c4e2
Yago-Zaragoza <yzarag@palermo.edu>
2025-09-25 15:58:42 -0300

    fix(components): add barrel export index.ts and update imports to use centralized exports for better Vercel resolution

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+// Component exports for better resolution
+export { default as VideoCard } from './VideoCard';
+export { default as CodeFrame } from './CodeFrame';
+export { default as IntroHero } from './IntroHero';
+export { default as ProgressRail } from './ProgressRail';
+export { default as SectionBackground } from './SectionBackground';
+export { default as ModernGradientBackground } from './ModernGradientBackground';
+export { default as PresentationCard } from './PresentationCard';
+export { default as ProgressDots } from './ProgressDots';
